6b91d77504dd6d65f2e88a1fe4593eef5a9ce4c6
angel <angel.cayo.morales@gmail.com>
2016-08-28 09:19:15 -0500

    La adición de algunos archivos nuevos

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NumberFun;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var generator = new FibonacciGenerator();
+
+            foreach (var digit in generator.Generate(15))
+            {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+hwapp
+
+Hola Mundo!
+
+28 08 2106 FibbonacciGenerator
+
+Chau.
